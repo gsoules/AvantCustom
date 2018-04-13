61cd18b69a70177e1d3bf5794ad198b42b32d137
gsoules <gsoules@avantlogic.com>
2018-04-13 18:14:36 -0400

    Simplify custom filters and functions.

--- AvantCustomPlugin.php
+++ AvantCustomPlugin.php
@@ -9,28 +9,22 @@ class AvantCustomPlugin extends Omeka_Plugin_AbstractPlugin
     protected $_filters = array(
         'fallback_image_name',
         'item_citation',
-        'item_thumbnail_class',
-        'item_thumbnail_header'
+        'item_thumbnail_class'
     );
 
     public function filterFallbackImageName($name, $args)
     {
-        return AvantCustom::getFallbackImageName($name, $args);
+        return AvantCustom::getFallbackImageName($name, $args['item']);
     }
 
     public function filterItemCitation($citation, $args)
     {
-        return AvantCustom::getItemCitation($citation, $args);
+        return AvantCustom::getItemCitation($citation, $args['item']);
     }
 
     public function filterItemThumbnailClass($class, $args)
     {
-        return AvantCustom::getItemThumbnailClass($class, $args);
-    }
-
-    public function filterItemThumbnailHeader($html, $args)
-    {
-        return AvantCustom::getItemThumbnailHeader($html, $args);
+        return AvantCustom::getItemThumbnailClass($class, $args['item']);
     }
 
     public function hookAdminHead($args)
