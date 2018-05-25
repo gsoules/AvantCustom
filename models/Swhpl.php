c231aae46a1f4902f79a6e60acf2a88e02dfd10a
gsoules <gsoules@avantlogic.com>
2018-05-25 17:49:41 -0400

    Remove unneeded methods.

--- models/Swhpl.php
+++ models/Swhpl.php
@@ -17,24 +17,9 @@ class Swhpl
         return $formattedDate;
     }
 
-    public static function getDefaultIdentifier(Item $item)
-    {
-        return self::getNextIdentifier();
-    }
-
     public static function getDefaultStatus(Item $item)
     {
         return 'OK';
-    }
-
-    protected static function getNextIdentifier()
-    {
-        $identifierElementId = ItemMetadata::getIdentifierElementId();
-        $db = get_db();
-        $sql = "SELECT MAX(CAST(text AS SIGNED)) AS next_element_id FROM `{$db->ElementTexts}` where element_id = $identifierElementId";
-        $result = $db->query($sql)->fetch();
-        $id = $result['next_element_id'] + 1;
-        return $id;
     }
 
     public static function saveItem($item)
@@ -91,7 +76,7 @@ class Swhpl
             $foundItem = $items[0];
             if ($savedItem->id != $foundItem->id)
             {
-                $nextElementId = self::getNextIdentifier();
+                $nextElementId = DigitalArchive::getNextIdentifier();
                 $elementName = ItemMetadata::getElementNameFromId($elementId);
                 AvantElements::addError($item, $elementName, "$text is used by another item. Next available Identifier is $nextElementId.");
             }
