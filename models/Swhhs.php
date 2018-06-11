94f05e076902750e5c24bd226e5107b89ffd9cdb
gsoules <gsoules@avantlogic.com>
2018-06-11 17:48:19 -0400

    Add support for both Finding Aid Ids and Omeka Identifiers.

--- models/Swhhs.php
+++ models/Swhhs.php
@@ -4,9 +4,10 @@ class Swhhs
 {
     public static function createSeeAlsoRelationshipNodes(Item $item, RelatedItemsTree $tree)
     {
-        $nodes = array();
         $label = 'See Also';
         $alsos = ItemMetadata::getElementTextForElementName($item, 'See Also');
+
+        $treeNode = new RelatedItemsTreeNode(0, $label);
 
         $references = array_map('trim', explode(',', $alsos));
 
@@ -15,7 +16,13 @@ class Swhhs
             $id = intval($reference);
             if ($id == 0)
                 continue;
-            $item = self::getItemFromFindingAidIs($id);
+
+            // Get the item from the Finding Aid Id. If that doesn't work, check to see if its an Identifier.
+            $item = self::getItemFromFindingAidId($id);
+            if (empty($item))
+            {
+                $item = ItemMetadata::getItemFromIdentifier($id);
+            }
             if (empty($item))
                 continue;
 
@@ -23,13 +30,14 @@ class Swhhs
             $relatedItem = ItemMetadata::getItemFromId($relatedItemId);
             if (empty($relatedItem))
                 continue;
-            $nodes[] = $tree->createCustomRelationshipsNode($relatedItem, $label);
+
+            $tree->addKidToRelatedItemsTreeNode($item, $item->id, $label, $treeNode);
         }
 
-        return $nodes;
+        return array($treeNode);
     }
 
-    protected static function getItemFromFindingAidIs($id)
+    protected static function getItemFromFindingAidId($id)
     {
         $elementId = ItemMetadata::getElementIdForElementName('Original Id');
         $items = get_records('Item', array('advanced' => array(array('element_id' => $elementId, 'type' => 'is exactly', 'terms' => $id))));
