58a675f67038242ff3478a7cba2a95e0d5e623cc
gsoules <gsoules@avantlogic.com>
2018-06-11 17:24:22 -0400

    Add logic to get item from Finding Aid Id.

--- models/Swhhs.php
+++ models/Swhhs.php
@@ -2,14 +2,39 @@
 
 class Swhhs
 {
-    public static function createSeeAlsoRelationshipNodes(RelatedItemsTree $tree)
+    public static function createSeeAlsoRelationshipNodes(Item $item, RelatedItemsTree $tree)
     {
+        $nodes = array();
         $label = 'See Also';
-        $relatedItemId = '188';
-        $relatedItem = ItemMetadata::getItemFromId($relatedItemId);
-        if (empty($relatedItem))
-            return array();
-        $customRelationshipsNode = $tree->createCustomRelationshipsNode($relatedItem, $label);
-        return array($customRelationshipsNode);
+        $alsos = ItemMetadata::getElementTextForElementName($item, 'See Also');
+
+        $references = array_map('trim', explode(',', $alsos));
+
+        foreach ($references as $reference)
+        {
+            $id = intval($reference);
+            if ($id == 0)
+                continue;
+            $item = self::getItemFromFindingAidIs($id);
+            if (empty($item))
+                continue;
+
+            $relatedItemId = $item->id;
+            $relatedItem = ItemMetadata::getItemFromId($relatedItemId);
+            if (empty($relatedItem))
+                continue;
+            $nodes[] = $tree->createCustomRelationshipsNode($relatedItem, $label);
+        }
+
+        return $nodes;
+    }
+
+    protected static function getItemFromFindingAidIs($id)
+    {
+        $elementId = ItemMetadata::getElementIdForElementName('Original Id');
+        $items = get_records('Item', array('advanced' => array(array('element_id' => $elementId, 'type' => 'is exactly', 'terms' => $id))));
+        if (empty($items))
+            return null;
+        return $items[0];
     }
 }
