0c6e1e4a239d5659fd064b1ac4f566a41952ca64
gsoules <gsoules@avantlogic.com>
2018-06-17 06:11:31 -0400

    Remove unused methods.

--- models/Swhpl.php
+++ models/Swhpl.php
@@ -17,16 +17,6 @@ class Swhpl
         return $formattedDate;
     }
 
-    public static function getDefaultStatus(Item $item)
-    {
-        return 'OK';
-    }
-
-    public static function saveItem($item)
-    {
-        return;
-    }
-
     public static function validateAccessDB($item, $accessDBValue)
     {
         $identifierElementName = ItemMetadata::getIdentifierElementName();
@@ -53,32 +43,6 @@ class Swhpl
             {
                 AvantElements::addError($item, 'Access DB', "This item did not come from MS Access. Choose 'Select Below' for Access DB.");
                 return;
-            }
-        }
-    }
-
-    public static function validateIdentifier($item, $elementId, $text)
-    {
-        // Make sure the value is an integer.
-        if (!ctype_digit($text))
-        {
-            AvantElements::addError($item, 'Identifier', 'Value must be a number consisting only of the digits 0 - 9.');
-            return true;
-        }
-
-        // Search the database to see if another Item has this identifier.
-        $items = get_records('Item', array( 'advanced' => array( array('element_id' => $elementId, 'type' => 'is exactly', 'terms' => $text ))));
-
-        if ($items)
-        {
-            // Found an Item with this identifier. Check if it's the Item being saved or another Item.
-            $savedItem = $item;
-            $foundItem = $items[0];
-            if ($savedItem->id != $foundItem->id)
-            {
-                $nextElementId = DigitalArchive::getNextIdentifier();
-                $elementName = ItemMetadata::getElementNameFromId($elementId);
-                AvantElements::addError($item, $elementName, "$text is used by another item. Next available Identifier is $nextElementId.");
             }
         }
     }
